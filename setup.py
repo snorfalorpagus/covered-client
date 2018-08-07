0f716793d50925a5af86d0a6b4cdd2b89b3140f1
Joshua Arnott <josh@snorfalorpagus.net>
2018-08-07 09:27:01 +0100

    Use setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup
 
 setup(
     name="covered-client",
+    use_scm_version=True,
     packages=["covered_client"],
     entry_points={
         "console_scripts": [
             "covered = covered_client.cli:main"
         ]
     },
+    setup_requires=[
+        "setuptools_scm"
+    ],
     install_requires=[
         "click",
         "coverage",
